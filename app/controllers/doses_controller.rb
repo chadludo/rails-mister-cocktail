6460c8088019b031e738b625937109379c9ad79f
Ludovic Villain <chadludo@gmail.com>
2020-11-20 12:22:36 +0100

    correct routes

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -21,7 +21,7 @@ class DosesController < ApplicationController
     @dose.destroy
 
     # no need for app/views/restaurants/destroy.html.erb
-    redirect_to cocktail_path
+    redirect_to cocktail_path(@dose.cocktail)
   end
 
   private
